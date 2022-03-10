a6c4ceae2a1734d03841e7051c043dd37122a607
Umut Saydam <umutsaydam24@gmail.com>
2022-03-10 16:08:46 +0300

    first commit

---
+++ Script.js
@@ -0,0 +1,39 @@
+var i = 1;
+
+function toDoAdd() {
+    var toDo = document.getElementById("toDo");
+
+    if (toDo.value != "") {
+        var toDoArr = toDo.value.split("-");
+
+        var styles = `
+        <div class="action" id="action">
+            <div class="checkbox-area">
+                <input type="checkbox" id="${i}" onClick="checkList(${i});">
+             </div>
+            <div class="action-list" id="${++i}">
+                <h3>${toDoArr[0]}</h3>
+                <p class="description">${(toDoArr[1] == undefined) ? "" : toDoArr[1]}</p>
+            </div>
+        </div>`;
+
+        let actionArea = document.getElementById("input-area");
+        actionArea.insertAdjacentHTML("Beforeend", styles);
+        i++;
+
+        toDo.value = "";
+    }
+}
+
+function checkList(action) {
+    var getId = document.getElementById(++action);
+    getId.style.textDecoration = "line-through";
+}
+
+function removeAll() {
+    var removeA = document.getElementById("action");
+    if (removeA) {
+        removeA.remove();
+    }
+}
+
